86a7894fa1511dc24b54890d331000b6d41ee46d
MSI-FW <softbaddog@gmail.com>
2021-09-11 11:08:06 +0800

    完成自定义窗口

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <TitleBar />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TitleBar from "./components/TitleBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    TitleBar,
+    HelloWorld,
+  },
+};
+</script>
+
+    <style>
+body,
+html {
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
+  height: 100%;
+}
+#app {
+  text-align: center;
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+  border: 1px solid #f5222d;
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,72 @@
+<template>
+<div class="titleBar">
+    <div class="title">
+        <div class="logo">
+            <img src="@/assets/logo.png">
+        </div>
+        <div class="txt">窗口标题</div>
+    </div>
+    <div class="windowTool">
+        <div @click="minisize">
+            <i class="iconfont iconminisize"></i>
+        </div>
+        <div v-if="isMaxSize" @click="restore">
+            <i class="iconfont iconrestore"></i>
+        </div>
+        <div v-else @click="maxsize">
+            <i class="iconfont iconmaxsize"></i>
+        </div>
+        <div @click="close" class="close">
+            <i class="iconfont iconclose"></i>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBar'
+}
+</script>
+
+<style lang="scss" scoped>
+@import url(https://at.alicdn.com/t/font_1378132_s4e44adve5.css);
+.titleBar {
+    height: 38px;
+    line-height: 36px;
+    background: #fff1f0;
+    display: flex;
+    border-bottom: 1px solid #f5222d;
+    .title {
+        flex: 1;
+        display: flex;
+        -webkit-app-region:drag;
+        .logo {
+            padding-left: 8px;
+            padding-right: 6px;
+            img { width: 20px; height: 20px; margin-top: 7px;}
+        }
+        .txt {
+            text-align: left;
+            flex: 1;
+        }
+    }
+    .windowTool {
+        div {
+            color: #888;
+            height: 100%;
+            width: 38px;
+            display: inline-block;
+            cursor: pointer;
+            i { font-size: 12px;}
+            &:hover { background: #ffccc7;}
+        }
+        .close:hover { color: #fff; background: #ff4d4f;}
+    }
+}
+.content {
+    flex: 1;
+    overflow-y: auto;
+    overflow-x: auto;
+}
+</style>
